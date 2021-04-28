a591ea818726418a94f23e6fd63712d62299a436
CoolSheff131 <a.nikulsheev@gmail.com>
2021-04-28 22:09:25 +0800

    убран лишний код

--- core/src/com/mygdx/game/cards/CardActor.java
+++ core/src/com/mygdx/game/cards/CardActor.java
@@ -23,21 +23,7 @@ public abstract class CardActor extends Group {
         img = new Image(skin,drawableName);
 
 
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("truetypefont/Amble-Light.ttf"));
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 30;
-        parameter.borderWidth = 1;
-        parameter.color = Color.YELLOW;
-        parameter.shadowOffsetX = 3;
-        parameter.shadowOffsetY = 3;
-        parameter.shadowColor = new Color(0, 0.5f, 0, 0.75f);
-        BitmapFont font24 = generator.generateFont(parameter); // font size 24 pixels
-        generator.dispose();
 
-        Label.LabelStyle labelStyle = new Label.LabelStyle();
-        labelStyle.font = font24;
-
-        
         lblName =  new Label(name,new Label.LabelStyle(new BitmapFont(), Color.BROWN));
         lblDescription =  new Label("Description",new Label.LabelStyle(new BitmapFont(), Color.BROWN));
         coem = new Image(skin,"coem");
